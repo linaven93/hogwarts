40f3762c8cc38be3f123b56ddd0093038fa6d94e
Lina Ven <linaven93@gmail.com>
2026-04-14 18:14:27 +0300

    addedd API fetch and log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("JS veikia");
+console.log("App started");
+
+fetch("https://hp-api.onrender.com/api/characters/students")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+
+    data.forEach((student) => {
+      console.log(student.name);
+    });
+  })
+  .catch((err) => console.error(err));
